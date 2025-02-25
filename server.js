8c5d8513153ffaced2bdf97eb4a4239e7acbd9f6
yogramming <yogeshdubey9336@gmail.com>
2025-02-26 01:57:17 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express")
+const app = express()
+const PORT = 8000
+
+const rappers = {
+    '21 savage': {
+        'age': 29,
+        'birthName': 'Like you would give a shit',
+        'birthPlace': 'London, England'
+    },
+    'chance the rapper': {
+        'age':28,
+        'birthName': 'Again the same shit',
+        'bithPlace': 'Bihar'
+    },
+    'dylan': {
+        'age': 29,
+        'birthName': 'For god sake! Again!!!',
+        'birthPlace': 'Bihar'
+    }
+
+}
+
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + "/index.html")
+})
+
+app.get('/api/:rapperName', (request, response) => {
+    const rapperName = request.params.rapperName.toLowerCase()
+    if (rappers[rapperName]) {
+        response.json(rappers[rapperName])
+    } else {
+        response.json(rappers['dylan'])
+    }
+})
+
+app.listen(PORT,() => {
+    console.log(`The server is running on ${PORT}! You better go catch it!`)
+})
