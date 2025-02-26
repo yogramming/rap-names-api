4a115be5092da3d9413a156b019f07f86881306b
yogramming <yogeshdubey9336@gmail.com>
2025-02-26 22:59:13 +0530

    Revert the changes

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ document.querySelector('button').addEventListener('click', apiRequest)
 async function apiRequest(){
     const rapperName = document.querySelector('input').value
     try{
-        const response = await fetch(`https://rap-names-api-xh76.onrender.com//api/${rapperName}`)
+        const response = await fetch(`https://rap-names-api-xh76.onrender.com/api/${rapperName}`)
         const data = await response.json()
 
         console.log(data)
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express")
+require('dotenv').config();
 const app = express()
 const PORT = 8000
 const cors = require("cors")
+const path = require("path")
 
 app.use(cors())
 
@@ -25,8 +27,14 @@ const rappers = {
 }
 
 app.get('/', (request, response) => {
-    response.sendFile(__dirname + "index.html")
+    response.sendFile(path.join(__dirname, "public", "index.html"))
 })
+
+// app.use(express.static("js"))
+
+// app.get('/', (request, response) => {
+//     response.sendFile(__dirname + "/js/main.js")
+// })
 
 app.get('/api/:rapperName', (request, response) => {
     const rapperName = request.params.rapperName.toLowerCase()
